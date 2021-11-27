e0cb5d72e4cd246ef4744b57bc81a108c057a183
m1stborn <r10922178@ntu.edu.tw>
2021-11-27 18:16:01 +0800

    Feat: basic http server

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+var (
+	port  = ":" + os.Getenv("PORT")
+	dbUri = os.Getenv("DB_URI")
+)
+
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprint(w, "Welcome!\n")
+}
 
 func main() {
-	fmt.Println("init")
+	fmt.Println(port, dbUri)
+
+	router := httprouter.New()
+
+	router.GET("/", Index)
+
+	//log.Fatal(http.ListenAndServe(":8080", router))
+	log.Fatal(http.ListenAndServe(port, router))
 }
