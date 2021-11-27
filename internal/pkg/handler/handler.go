8386bb4881a2e3e9c25ce6aca631cdce62a9db09
m1stborn <r10922178@ntu.edu.tw>
2021-11-27 22:54:00 +0800

    Feat: birthday greeting API version 2

--- internal/pkg/handler/handler.go
+++ internal/pkg/handler/handler.go
@@ -10,9 +10,17 @@ import (
 )
 
 func HandleBdayGreet(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	maleMsg := "We offer special discount 20%% for the following items:\nWhite Wine, iPhone X\n"
+	femaleMsg := "We offer special discount 50%% for the following items:\nCosmetic, LV Handbags\n"
+
 	users, _ := model.DB.GetUserByDate(8, 8)
+
 	for _, user := range users {
 		replyMsg := fmt.Sprintf("Subject: Happy birthday!\nHappy birthday, dear %s!\n", user.FirstName)
-		fmt.Fprintf(w, replyMsg)
+		if user.Gender == "Male" {
+			fmt.Fprintf(w, replyMsg+maleMsg)
+		} else {
+			fmt.Fprintf(w, replyMsg+femaleMsg)
+		}
 	}
 }
