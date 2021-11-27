f5f9ff6635fa70131e27144618a85e4d49740eba
m1stborn <r10922178@ntu.edu.tw>
2021-11-28 01:23:44 +0800

    Feat: birthday greeting API version 3 (serve with image)

--- internal/pkg/handler/handler.go
+++ internal/pkg/handler/handler.go
@@ -2,17 +2,67 @@ package handler
 
 import (
 	"fmt"
+	ht "html/template"
 	"net/http"
+	"time"
 
 	"github.com/m1stborn/BdayGreet/internal/pkg/model"
 
 	"github.com/julienschmidt/httprouter"
 )
 
+var ImageTemplate string = `<!DOCTYPE html>
+<html>
+<head>
+    <title>Birthday Greeting</title>
+</head>
+<body>
+	{{ range .Users }}
+		<div>
+		Subject: Happy birthday!
+		</div>
+		<div>
+		Happy birthday, dear {{.Name}}
+		</div>
+		{{ if .AgeGt49 }}
+			<img src="https://imgur.com/NgkfJJv.jpg" width="20%" height="20%">
+		{{ end }}
+	{{ end }}
+</body>
+</html>`
+
+type HtmlUser struct {
+	Name    string
+	AgeGt49 bool
+}
+
 func HandleBdayGreet(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	users, _ := model.DB.GetUserByDate(8, 8)
+	users, _ := model.DB.GetUserByDate(12, 22)
+	var names []HtmlUser
+	var gt49 bool
+
 	for _, user := range users {
-		replyMsg := fmt.Sprintf("Subject: Happy birthday!\nHappy birthday, dear %s!\n", user.FirstName)
-		fmt.Fprintf(w, replyMsg)
+		now := time.Now()
+		dur := now.Sub(user.Birth)
+		age := dur.Seconds() / 31207680
+		if age > 49 {
+			gt49 = true
+		} else {
+			gt49 = false
+		}
+		names = append(names, HtmlUser{
+			Name:    user.FirstName,
+			AgeGt49: gt49,
+		})
 	}
+	t, tErr := ht.New("webpage").Parse(ImageTemplate)
+	if tErr != nil {
+		fmt.Println(tErr)
+	}
+	tErr = t.Execute(w, struct {
+		Users []HtmlUser
+	}{
+		names,
+	})
+
 }
