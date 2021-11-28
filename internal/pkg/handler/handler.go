a0d5541de9f6065ffb56a635c5920e35b47eb96a
m1stborn <r10922178@ntu.edu.tw>
2021-11-28 16:01:12 +0800

    Feat: birthday greeting API version 5 (different data format)

--- internal/pkg/handler/handler.go
+++ internal/pkg/handler/handler.go
@@ -1,6 +1,8 @@
 package handler
 
 import (
+	"encoding/json"
+	"encoding/xml"
 	"fmt"
 	"net/http"
 
@@ -9,10 +11,55 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
-func HandleBdayGreet(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+type greetMsg struct {
+	Title   string
+	Content string
+}
+
+func HandleBdayGreetJson(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	var msgs []greetMsg
+
 	users, _ := model.DB.GetUserByDate(8, 8)
+
 	for _, user := range users {
-		replyMsg := fmt.Sprintf("Subject: Happy birthday!\nHappy birthday, dear %s!\n", user.FirstName)
-		fmt.Fprintf(w, replyMsg)
+		title := fmt.Sprintf("Subject: Happy birthday!")
+		content := fmt.Sprintf("Happy birthday, dear %s!", user.FirstName)
+		msgs = append(msgs, greetMsg{Title: title, Content: content})
 	}
+	if len(msgs) == 0 {
+		msgs = make([]greetMsg, 0)
+	}
+
+	js, err := json.Marshal(msgs)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
+
+func HandleBdayGreetXml(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	var msgs []greetMsg
+
+	users, _ := model.DB.GetUserByDate(8, 8)
+
+	for _, user := range users {
+		title := fmt.Sprintf("Subject: Happy birthday!")
+		content := fmt.Sprintf("Happy birthday, dear %s!", user.FirstName)
+		msgs = append(msgs, greetMsg{Title: title, Content: content})
+	}
+	if len(msgs) == 0 {
+		msgs = make([]greetMsg, 0)
+	}
+
+	x, err := xml.MarshalIndent(msgs, "", "	")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/xml")
+	w.Write(x)
 }
