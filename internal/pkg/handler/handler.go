cf5666a15e77cbb3ad98889f6a062b3ac9a8f718
m1stborn <r10922178@ntu.edu.tw>
2021-12-03 02:06:56 +0800

    Feat: add unit test for handler

--- internal/pkg/handler/handler.go
+++ internal/pkg/handler/handler.go
@@ -9,11 +9,14 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
+var GetUSerByData = model.DB.GetUserByDate
+
 func HandleBdayGreet(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	maleMsg := "We offer special discount 20%% for the following items:\nWhite Wine, iPhone X\n"
 	femaleMsg := "We offer special discount 50%% for the following items:\nCosmetic, LV Handbags\n"
 
-	users, _ := model.DB.GetUserByDate(8, 8)
+	//users, _ := model.DB.GetUserByDate(8, 8)
+	users, _ := GetUSerByData(8, 8)
 
 	for _, user := range users {
 		replyMsg := fmt.Sprintf("Subject: Happy birthday!\nHappy birthday, dear %s!\n", user.FirstName)
